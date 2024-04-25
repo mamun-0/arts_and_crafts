cc65e75193d2a2757454822c53082feaa02baa5b
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-04-25 16:55:03 +0600

    wrap with authprovider of router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import 'animate.css';
+import { AuthProvider } from "../AuthProvider/AuthProvider";
+import { RouterProvider } from "react-router-dom";
+import { router } from "../router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1 className="text-4xl text-center font-bold">Hello world!</h1>
+  <AuthProvider>
+    <RouterProvider router={router}></RouterProvider>
+  </AuthProvider>
 );
