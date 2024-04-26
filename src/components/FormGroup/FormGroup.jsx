c988edb77e21fecf26d3c03d7d06ba993cf82d27
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-04-26 14:27:21 +0600

    fix form-group

--- src/components/FormGroup/FormGroup.jsx
+++ src/components/FormGroup/FormGroup.jsx
@@ -1,13 +1,12 @@
 export function FormGroup({
   errorMessage = "",
   children,
-  position = "static",
 }) {
   return (
-    <div className={position}>
+    <div className="flex flex-col">
       {children}
       {errorMessage.length > 0 && (
-        <small className="text-red-700">{errorMessage}</small>
+        <small className="text-red-700 block">{errorMessage}</small>
       )}
     </div>
   );
