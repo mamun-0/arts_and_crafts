f672dcf00aa5b3d27d2f12ccc10411af62db45ed
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-04-26 14:28:18 +0600

    add craft page

---
+++ src/Pages/AddCraft/AddCraft.jsx
@@ -0,0 +1,11 @@
+import { Craftitem } from "../../components/CraftItem/Craftitem";
+import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
+
+export function AddCraft() {
+  return (
+    <div>
+      <SectionTitle title="Add Item" />
+      <Craftitem />
+    </div>
+  );
+}
