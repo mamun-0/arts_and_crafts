a2dae7dd2ec10e76804fb5193404038f50c73793
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-04-27 09:17:07 +0600

    tabular view of all arts

---
+++ src/components/ArtAndCraftTable/ArtAndCraftTable.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+export function ArtAndCraftTable(props) {
+  const allArtsAndCrafts = props?.data;
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Item</th>
+              <th>Subcategory</th>
+              <th>
+                <span className="text-2xl font-bold">&#2547;</span> price
+              </th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row starts*/}
+
+            {allArtsAndCrafts && allArtsAndCrafts?.length ? (
+              allArtsAndCrafts.map((item, idx) => {
+                const { item_name, subcategory_name, price } = item;
+                return (
+                  <tr key={idx}>
+                    <th>{idx + 1}</th>
+                    <td>{item_name}</td>
+                    <td>{subcategory_name}</td>
+                    <td>{price}</td>
+                    <td>
+                      <Link className="btn btn-sm">View Details</Link>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr className="text-center font-bold text-red-500 text-2xl">
+                <td className="col-span-3">Empty List</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
