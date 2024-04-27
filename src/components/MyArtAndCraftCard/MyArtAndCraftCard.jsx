ef27da75c2a54c1eea50e3335419380c1df2d374
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-04-27 09:20:55 +0600

    my art and craft

---
+++ src/components/MyArtAndCraftCard/MyArtAndCraftCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { DeleteModal } from "../DeleteModal/DeleteModal";
+
+export function MyArtAndCraftCard({ myArtsAndCarts, setToggle }) {
+  return (
+    <>
+      {myArtsAndCarts.map((item, idx) => {
+        const { image, item_name, price, rating, customization, stockStatus } =
+          item;
+        return (
+          <div key={idx}>
+            <div className="border bg-white border-gray-200 rounded-lg p-2 shadow-xl hover:scale-[1.01] transition-transform duration-600">
+              <img
+                className="h-52 w-full object-cover rounded-lg"
+                src={image}
+                alt="cart-heading-image"
+              />
+              <ul className="space-y-2 p-2">
+                <li className="border-b py-1">Item : {item_name}</li>
+                <li className="border-b py-1">
+                  Price: {price} <span className="text-2xl">&#x09F3;</span>
+                </li>
+                <li className="border-b py-1">Rating: {rating}</li>
+                <li className="border-b py-1">
+                  Customization: {customization}
+                </li>
+                <li className="border-b py-1">Stock Status: {stockStatus}</li>
+              </ul>
+              <div className="text-right">
+                <Link
+                  className="btn btn-info btn-sm"
+                  to={`/my-art-craft-list/${item._id}`}
+                >
+                  Update
+                </Link>
+                <DeleteModal id={item._id} setToggle={setToggle} />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
