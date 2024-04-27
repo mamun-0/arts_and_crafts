4c3c0e51f5e7606f951984d83be24a036bbb703a
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-04-27 09:19:14 +0600

    delete modal

---
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export function DeleteModal({ id, setToggle }) {
+  function deletePost(id) {
+    axios
+      .delete(`${import.meta.env.VITE_BASE_URL}/my-art-craft-list/${id}`)
+      .then((res) => {
+        toast.success("Deleted Successfully!");
+        setToggle();
+      })
+      .catch(() => {
+        toast.error("Deletion failed!");
+      });
+  }
+  return (
+    <div>
+      <button
+        className="btn btn-error btn-sm"
+        onClick={() => document.getElementById("my_modal_1").showModal()}
+      >
+        Delete
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Are You sure to delete this item🤔🤔!
+          </h3>
+          <p className="py-4">
+            Press ESC key or click the <span className="font-black text-green-600">No</span> button to cancel deletion
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button
+                onClick={() => {
+                  deletePost(id);
+                }}
+                className="btn btn-error"
+              >
+                Yes
+              </button>
+            </form>
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-accent">No</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+}
